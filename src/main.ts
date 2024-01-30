1b2b021fb251764f89aa2e70ca566889436c04a2
Anne <anneris.wong@gmail.com>
2024-01-30 12:36:20 -0500

    Add Tailwind CSS, implement template styles, and configure Vite setup.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import "@fontsource/libre-baskerville";
+import "@fontsource/libre-baskerville/700.css";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "dark-gray": "#7D7D7D",
+        "light-gray": "#E5E5E5",
+        "almost-white": "#F3F3F3",
+      },
+      fontFamily: {
+        custom: ["Libre Baskerville", "sans-serif"],
+      },
+      fontSize: {
+        "9xl": ["12.5rem", "9.375rem"],
+        "6xl": ["3.5rem", "4rem"],
+        "4xl": ["1.5rem", "1.8125rem"],
+        "3xl": ["1.125rem", "1.375rem"],
+        "2xl": [".9375rem", "1.1875rem"],
+        xl: [".875rem", "1.75rem"],
+        lg: [".8125rem", "1.0625rem"],
+        base: [".75rem", ".9375rem"],
+        sm: ".625rem",
+        xs: [".5625rem", ".5rem"],
+      },
+      letterSpacing: {
+        widest: ".15625em",
+        wider: ".13393em",
+        wide: ".125em",
+      },
+      backdropOpacity: {
+        75: ".7546",
+      },
+      padding: {
+        "29px": "29px",
+        "27px": "27px",
+        "23px": "23px",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
