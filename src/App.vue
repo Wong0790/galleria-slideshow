05816e6666c6ca8722191c020d83024629d1f698
Anne <anneris.wong@gmail.com>
2024-01-30 17:08:52 -0500

    Update assets folder, update application data, add gallery images and first styles

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import LogoIcon from "@icons/LogoIcon.vue";
+import GalleryCard from "@components/GalleryCard.vue";
+import { paintings } from "./data/app-data";
 </script>
 
 <template>
@@ -8,6 +10,13 @@ import LogoIcon from "@icons/LogoIcon.vue";
     <button class="btn btn1">Start slideshow</button>
   </header>
   <section class="gallery">
-    <h1>Donec nec justo eget felis facilisis</h1>
+    <GalleryCard
+      v-for="(painting, index) in paintings"
+      :key="index"
+      :src="painting.images.thumbnail"
+      :alt="painting.name"
+      :title="painting.name"
+      :artist="painting.artist.name"
+    />
   </section>
 </template>
---
+++ src/components/GalleryCard.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+defineProps({
+  src: String,
+  alt: String,
+  title: String,
+  artist: String,
+});
+</script>
+
+<template>
+  <div class="card">
+    <img :src="src" :alt="alt" loading="lazy" decoding="async" />
+    <div class="foreground">
+      <div class="content-container">
+        <h2>{{ title }}</h2>
+        <small>{{ artist }}</small>
+      </div>
+    </div>
+  </div>
+</template>
