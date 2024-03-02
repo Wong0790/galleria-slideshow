3db19802f2277284b55ed51daa0b1c4735ec3e4d
Anne <anneris.wong@gmail.com>
2024-03-02 12:51:59 -0500

    update vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  /*assetsInclude: ["assets"],*/
+  assetsInclude: ["images-1", "images-2"],
   plugins: [vue()],
   server: {
     port: 3000,
